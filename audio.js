5d4912c01f1bc4a1d1fbd307bfd139d895507c57
RJ Marsan <me@rj.io>
2012-12-03 02:44:30 -0600

    A finished attempt1

--- audio.js
+++ audio.js
@@ -14,21 +14,31 @@ function Audio() {
     this.ready = false;
     this.audiolet = new Audiolet();
     this.envelope = new ADSREnvelope(this.audiolet, 0, 2, 0.1, 1, 2);
-    this.gain = new Gain(this.audiolet, 1);
-    this.reverb = new Reverb(this.audiolet, 0.7, 0.8, 0.2);
-    this.volume = new Gain(this.audiolet, 0.5);
-    this.onoff = new Gain(this.audiolet, 1);
+    this.gain = new Gain(this.audiolet, 0);
+    this.reverb = new Reverb(this.audiolet, 0.8, 0.8, 0.2);
+    this.delay = new FeedbackDelay(this.audiolet, 0.5, 0.5, 0.25, 0.3);
+    this.volume = new Gain(this.audiolet, 0);
+    this.onoff = new Gain(this.audiolet, 0);
     this.out = this.gain;
 
-    this.gain.connect(this.reverb);
+    this.gain.connect(this.delay);
+    this.delay.connect(this.reverb);
     this.envelope.connect(this.gain, 0, 1);
     this.reverb.connect(this.volume);
     this.volume.connect(this.audiolet.output);
 
 
-    this.Synth = function(audiolet, frequency, vol) {
+    this.Synth = function(audiolet, frequency, vol, type) {
         AudioletGroup.apply(this, [audiolet, 0, 1]);
-        this.sine = new Sine(this.audiolet, frequency);
+
+        if (type == 0) 
+            this.sine = new Sine(this.audiolet, frequency);
+        else if (type == 1) 
+            this.sine = new Sine(this.audiolet, frequency);
+        else if (type == 2) 
+            this.sine = new Triangle(this.audiolet, frequency);
+        else  
+            this.sine = new Triangle(this.audiolet, frequency);
         this.gain = new Gain(this.audiolet);
         this.gain2 = new Gain(this.audiolet);
         this.gain2.gain.setValue(vol);
@@ -49,7 +59,7 @@ function Audio() {
     this.Noise = function(audiolet) {
         AudioletGroup.apply(this, [audiolet, 0, 1]);
         this.noise = new WhiteNoise(this.audiolet);
-        this.noisefilter = new BandPassFilter(this.audiolet, 1600);
+        this.noisefilter = new BandPassFilter(this.audiolet, 100);
         this.noisegain = new Gain(this.audiolet, 0.4);
         
         this.noise.connect(this.noisefilter);
@@ -63,9 +73,9 @@ function Audio() {
     this.ready = true;
 }
 
-Audio.prototype.ping = function(midinote, volume) {
+Audio.prototype.ping = function(midinote, volume, type) {
     var freq = Math.pow(2, (midinote-69)/12)*440;
-    var synth = new this.Synth(this.audiolet, freq, volume);
+    var synth = new this.Synth(this.audiolet, freq, volume, type);
     synth.connect(this.out);
 };
 
@@ -82,12 +92,14 @@ Audio.prototype.updateTime = function(hourinweek) {
     hour = (hour+11)%24; //shift over
     hour = Math.abs(12-hour)*2;
 
+
     var dayinweek = Math.floor(hourinweek/7);
-    var hourfreq = 200+hour*50;
+    var hournote = 24+hour*2;
+    var hourfreq = Math.pow(2, (hournote-69)/12)*440;
     this.noise.noisefilter.frequency.setValue(hourfreq);
     var volume = (1-hour/24)*0.7 + 0.3;
     this.noise.noisegain.gain.setValue(volume);
-    //console.log("Setting hour "+hourinweek+ " to "+hour+" and "+hourfreq+" and "+volume);
+    console.log("Setting hour "+hourinweek+ " to "+hour+" and "+hourfreq+" and "+volume);
 };
 
 
@@ -137,6 +149,22 @@ function Sonification() {
             audio.ping(offset+note, vol);
         }
     }
+
+    this.ping = function(x1, y1, x2, y2, vel, ismain, week) {
+        var x = (x1+x2)/2;
+        var y = (y1+y2)/2;
+        if (x < 0 || x > 1 || y < 0 || y > 1) return;
+        var vel = Math.min(1, Math.abs(vel));
+        var velindex = Math.floor(vel*notes.length);
+        var note = notes[velindex];
+        var range = 3-(week)%4;
+        var offset = (range+3)*12;
+        var type = 3-range; //square, saw, etc.
+        console.log("offset: "+offset+" for range: "+range+" and type: "+type);
+        if (audio && audio.ready) {
+            audio.ping(offset+note, Math.sqrt(vel), type);
+        }
+    }
 }
 
 
@@ -144,12 +172,12 @@ function Sonification() {
 var notes = [0, 5, 7, 9, 12, 17, 19, 21, 24, 29, 31, 33, 36];
 var notes = [0, 3, 5, 9];
 var origlength = notes.length;
-for (var x=0; x<origlength*2+1; x++) {
+for (var x=0; x<origlength*1.5; x++) {
     notes.push(notes[x]+12);
 }
-notes = notes.concat(notes);
-notes = notes.concat(notes);
-notes = notes.concat(notes);
+//notes = notes.concat(notes);
+//notes = notes.concat(notes);
+//notes = notes.concat(notes);
 console.log(notes);
 function makeGrid() {
     var notegrid = [];
